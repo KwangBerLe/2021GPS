d4fa08178e82c3ef11ac400a39e9b56dafe7c9c2
KwangBerLe <gamj4525@gmail.com>
2021-04-02 17:13:41 +0900

    20200402

---
+++ Dodge_Game/Assets/Scripts/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private int directionVector = 1;
+    private int speed = 7;
+
+    public void SetDirectionVector(int directionVector)
+    {
+        this.directionVector = directionVector;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x += directionVector * speed * Time.deltaTime;
+        transform.position = pos;
+
+        if(pos.x < -10 || pos.x > 10)
+        {
+            gameObject.SetActive(false);
+            //Destroy(gameObject);  
+            //SetActive 가 계속 쌓이면 렉이 걸릴 수 있기 때문에, 적절한 상황에 SetActive와 Destroy 를 섞어가며 사용하자.
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Player"))
+        {
+            //Destroy(gameObject);
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Dodge_Game/Assets/Scripts/Player.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int hp = 3; //외부에서 접근하여 수정하는 것을 불허함.
+    //public float playerSpeed = 5.0f;
+    void Start()
+    {
+        
+    }
+
+    public int GetHP() // Getter
+    {
+        return hp;
+    }
+
+    public void SetHP(int hp) // Setter
+    {
+        this.hp = hp;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Enemy"))
+        {
+            hp -= 1;
+            print(hp);
+        }
+    }
+
+    void Update()
+    {
+        if (hp > 0)
+        {
+
+            float posX = 0, posY = 0;
+            //if(Input.GetKey(KeyCode.A))
+            //{
+            //    posX -= 1;
+            //}
+
+            //if (Input.GetKey(KeyCode.D))
+            //{
+            //    posX += 1;
+            //}
+
+            //if (Input.GetKey(KeyCode.W))
+            //{
+            //    posY += 1;
+            //}
+
+            //if (Input.GetKey(KeyCode.S))
+            //{
+            //    posY -= 1;
+            //}
+
+            posX = Input.GetAxis("Horizontal");
+            posY = Input.GetAxis("Vertical");
+
+            Vector3 pos = gameObject.transform.position; // Inspector 에서 본 Transform이 맞습니다.
+            pos.x += posX * Time.deltaTime * 10;
+            pos.y += posY * Time.deltaTime * 10;
+
+            if (pos.x < -8.3f)
+            {
+                pos.x = -8.3f;
+            }
+            else if (pos.x > 8.3f)
+            {
+                pos.x = 8.3f;
+            }
+            if (pos.y < -4.7f)
+            {
+                pos.y = -4.7f;
+            }
+            else if (pos.y > 4.7f)
+            {
+                pos.y = 4.7f;
+            }
+
+            gameObject.transform.position = pos;
+        }
+    }
+}
